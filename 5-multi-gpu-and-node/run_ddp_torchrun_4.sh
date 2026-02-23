7a626e5a988895e3b78649d9870f76b1d928a1aa
Marlon Tobaben <241949331+marlon-tobaben@users.noreply.github.com>
2026-02-23 15:55:25 +0200

    feat: add new container

--- 5-multi-gpu-and-node/run_ddp_torchrun_4.sh
+++ 5-multi-gpu-and-node/run_ddp_torchrun_4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=project_xxxxxxxxx
+#SBATCH --account=project_462000131
 #SBATCH --partition=standard-g
 #SBATCH --nodes=4
 #SBATCH --gpus-per-node=8
@@ -10,11 +10,12 @@
 #SBATCH --mem=480G
 
 # this module facilitates the use of singularity containers on LUMI
-module use  /appl/local/containers/ai-modules
-module load singularity-AI-bindings
+module purge
+module use /appl/local/laifs/modules
+module load lumi-aif-singularity-bindings
 
-# choose container that is copied over by set_up_environment.sh
-CONTAINER=../resources/lumi-pytorch-rocm-6.2.4-python-3.12-pytorch-v2.7.0.sif
+# choose container
+SIF=/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260216_093549/lumi-multitorch-full-u24r64f21m43t29-20260216_093549.sif
 
 # Tell RCCL to use Slingshot interfaces and GPU RDMA
 export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
@@ -23,5 +24,4 @@ export NCCL_NET_GDR_LEVEL=PHB
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 export MASTER_PORT=29500
 
-export SINGULARITYENV_PREPEND_PATH=/user-software/bin
-srun singularity exec -B ../resources/visiontransformer-env.sqsh:/user-software:image-src=/ $CONTAINER bash -c 'python -m torch.distributed.run --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=8 --rdzv_id=\$SLURM_JOB_ID --rdzv_backend=c10d --rdzv_endpoint="$MASTER_ADDR:$MASTER_PORT" ddp_visiontransformer.py'
+srun singularity run $SIF bash -c 'source /scratch/project_462000131/marlonto/LUMI-AI-Guide/resources/ai-guide-env/bin/activate && python -m torch.distributed.run --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=8 --rdzv_id=\$SLURM_JOB_ID --rdzv_backend=c10d --rdzv_endpoint="$MASTER_ADDR:$MASTER_PORT" ddp_visiontransformer.py'
