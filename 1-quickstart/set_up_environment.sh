aa6fbf388c1229fb1580f8e4fd43937493f09575
Marlon Tobaben <241949331+marlon-tobaben@users.noreply.github.com>
2026-03-05 13:52:45 +0200

    fix: add sqfs file copying and doc to quickstart

--- 1-quickstart/set_up_environment.sh
+++ 1-quickstart/set_up_environment.sh
@@ -3,3 +3,5 @@
 # Please have a look at the terms of access (https://www.image-net.org/download.php) before using the dataset
 echo "Copying training data to ../resources/ directory."
 cp /appl/local/training/LUMI-AI-Guide/tiny-imagenet-dataset.hdf5 ../resources/train_images.hdf5
+echo "Copying sqsh virtual environment to ../resources/ directory."
+cp /appl/local/training/LUMI-AI-Guide/ai-guide-env.sqsh ../resources/ai-guide-env.sqsh
