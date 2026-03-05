b91724f6584fa4d73cb288d386e4a0dd76c14546
Marlon Tobaben <241949331+marlon-tobaben@users.noreply.github.com>
2026-03-05 13:53:19 +0200

    fix: use ai-guide-env also for mlflow

--- 8-MLflow-visualization/run_ddp_torchrun.sh
+++ 8-MLflow-visualization/run_ddp_torchrun.sh
@@ -29,4 +29,4 @@ export NCCL_NET_GDR_LEVEL=PHB
 export SINGULARITYENV_PREPEND_PATH=/user-software/bin # gives access to packages inside the container
 
 # need to create sqsh file with mlflow included
-srun singularity run -B ../resources/ai-guide-ml-flow-env.sqsh:/user-software:image-src=/ $SIF bash -c 'python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 mlflow_ddp_visiontransformer.py'
+srun singularity run -B ../resources/ai-guide-env.sqsh:/user-software:image-src=/ $SIF bash -c 'python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 mlflow_ddp_visiontransformer.py'
