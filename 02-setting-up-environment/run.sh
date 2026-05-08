619c93c7e61eb9306406bfd84fa41974b43b4245
Marlon Tobaben <241949331+marlon-tobaben@users.noreply.github.com>
2026-05-08 12:43:45 +0300

    feat: add scripts to create and test squashfs extension

---
+++ 02-setting-up-environment/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=project_xxxxxxxxx  # project account to bill 
+#SBATCH --partition=dev-g            # other options are small-g and standard-g
+#SBATCH --gpus-per-node=1            # Number of GPUs per node (max of 8)
+#SBATCH --ntasks-per-node=1          # Use one task for one GPU
+#SBATCH --cpus-per-task=7            # Use 1/8 of all available 56 CPUs on LUMI-G nodes
+#SBATCH --mem-per-gpu=60G            # CPU RAM per GPU (GPU memory is always 64GB per GPU)
+#SBATCH --time=0:15:00               # time limit
+
+# this module facilitates the use of singularity containers on LUMI
+module purge
+module use /appl/local/laifs/modules
+module load lumi-aif-singularity-bindings
+
+SIF=/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260415_130625/lumi-multitorch-full-u24r70f21m50t210-20260415_130625.sif
+
+export SINGULARITYENV_PREPEND_PATH=/user-software/bin # gives access to packages inside the container
+singularity run -B optuna-env.sqsh:/user-software:image-src=/ $SIF python test_squashfs.py
